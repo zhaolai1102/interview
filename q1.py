643cbc1e43164043216215eac5438618d987321d
zhaolai1102 <zhaolai1102@163.com>
2020-03-24 09:02:47 +0800

    完成

--- q1.py
+++ q1.py
@@ -19,4 +19,18 @@
 
 def how_many_ways(digitarray):
     # implement here
-    
+    n = 1
+    def one_or_two(digit_array):
+        nonlocal n
+        if len(digit_array) == 1:
+            return
+        one_or_two(digit_array[1:])
+        if digit_array[0] == '1' or digit_array[0] == '2' and digit_array[1] < '6':
+            n += 1
+            if len(digit_array) > 2:
+                one_or_two(digit_array[2:])
+    one_or_two(digitarray)
+    return n
+
+if __name__ == '__main__':
+    print(how_many_ways('0219'))
